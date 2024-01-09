8924290366a39d02b67a090521cd3fb019305d19
ioneluoooo <creciunion4@gmail.com>
2024-01-09 16:29:26 +0200

    Microservice

---
+++ admin/src/app.ts
@@ -0,0 +1,137 @@
+import * as express from 'express';
+import { DataSource, Db } from 'typeorm';
+import * as cors from 'cors';
+import { Product } from './entity/product';
+import * as amqp from "amqplib/callback_api"
+
+const dataSource = new DataSource({
+    "type": "mysql",
+    "host": "localhost",
+    "port": 3306,
+    "username": "root",
+    "password": "pHih5/F[(0:9",
+    "database": "nodeapi_database",
+    "entities": [
+        "src/entity/*.js"
+    ],
+    "logging": true,
+    "synchronize": true
+});
+
+//connecting to our database
+dataSource.initialize()
+    .then(async (db) => {
+
+        //Connecting to rabbitMQ
+        amqp.connect('amqps://dmxrsfku:z3953IdAtawCGTJ4aVyf8pVNWOV9cdNi@rattlesnake.rmq.cloudamqp.com/dmxrsfku', (error, connection) => {
+            if (error) {
+                throw error
+            }
+
+            connection.createChannel((err, channel) => {
+                if (err) {
+                    throw err
+                }
+
+                const app = express();
+
+                app.use(cors({
+                    origin: ['http://localhost:3000']
+                }));
+
+                app.use(express.json());
+
+                const productRepository = db.getRepository(Product)
+                const PORT = 3000;
+
+                app.listen(PORT, () => {
+                    console.log(`Server is launched at port ${PORT}`);
+                    console.log('Database connected');
+                });
+
+                app.get('/api/products', async (req, res) => {
+                    const products = await productRepository.find()
+
+                    res.json(products)
+                })
+
+                app.post('/api/products', async (req, res) => {
+                    const product = await productRepository.create(
+                        req.body
+                    )
+                    const result = await productRepository.save(product)
+
+                    // initializing an event
+                    // setting the product
+                    channel.sendToQueue('product_created', Buffer.from(JSON.stringify(result)))
+
+                    return res.send(result)
+                })
+
+                app.post('/api/products/:id/like', async (req, res) => {
+                    const productId: any = req.params.id
+
+                    const product = await productRepository.findOne({ where: { id: productId } })
+
+                    product.likes++
+
+                    const result = await productRepository.save(product)
+                    return res.send(result)
+
+                })
+
+
+                app.get('/api/products/:id', async (req, res) => {
+                    const productId: any = req.params.id
+                    const product = await productRepository.findOne({ where: { id: productId } })
+                    if (product) {
+                        res.send(product)
+                    } else {
+                        res.status(404).json({ message: "Product not found" })
+                    }
+                })
+
+                app.put('/api/products/:id', async (req, res) => {
+                    const productId: any = req.params.id
+                    const product = await productRepository.findOne({ where: { id: productId } })
+
+                    productRepository.merge(product, req.body)
+                    const result = await productRepository.save(product)
+
+                     // initializing an event
+                    // setting the product
+                    channel.sendToQueue('product_updated', Buffer.from(JSON.stringify(result)))
+
+                    return res.send(result)
+                })
+
+                app.delete('/api/products/:id', async (req, res) => {
+                    const productId: any = req.params.id;
+
+                    const product = await productRepository.findOne({ where: { id: productId } });
+
+                    if (!product) {
+                        return res.status(404).json({ message: 'Product not found' });
+                    }
+
+                    const deleteResult = await productRepository.delete(product);
+
+                     // initializing an event
+                    // setting the product
+                    channel.sendToQueue('product_deleted', Buffer.from(JSON.stringify(product)))
+
+                    res.send(deleteResult);
+                });
+
+                process.on('beforeExit', () => {
+                    console.log('closing')
+                    connection.close()
+                })
+            })
+        })
+    })
+    .catch((err) => {
+        console.log('Could not connect to the database', err)
+    })
+
+
---
+++ main/src/app.ts
@@ -0,0 +1,127 @@
+import * as express from "express"
+import * as cors from "cors"
+import { DataSource } from "typeorm"
+import * as amqp from "amqplib/callback_api"
+import { Product } from "./entity/product";
+import axios from "axios"
+
+const dataSource = new DataSource({
+    "type": "mongodb",
+    "url": "mongodb+srv://admin:1ws2qa3ed@moicluster.iygrrhw.mongodb.net/nodeapi_database?retryWrites=true&w=majority",
+    "synchronize": true,
+    "logging": true,
+    "entities": [
+        "src/entity/*.js"
+    ]
+});
+
+dataSource.initialize()
+    .then(async (db) => {
+
+        const productRepository = db.getMongoRepository(Product)
+
+        amqp.connect('amqps://dmxrsfku:z3953IdAtawCGTJ4aVyf8pVNWOV9cdNi@rattlesnake.rmq.cloudamqp.com/dmxrsfku', (error, connection) => {
+            if (error) {
+                throw error
+            }
+
+            connection.createChannel((err, channel) => {
+                if (err) {
+                    throw err
+                }
+
+                // getting the events
+                channel.assertQueue('product_created', { durable: true })
+                channel.assertQueue('product_updated', { durable: true })
+                channel.assertQueue('product_deleted', { durable: true })
+
+                const app = express()
+
+                app.use(cors())
+
+                app.use(express.json())
+
+                // getting the message
+                channel.consume('product_created', async (msg) => {
+                    const eventProduct: Product = JSON.parse(msg.content.toString())
+
+                    const product = new Product()
+                    product.admin_id = parseInt(eventProduct.id)
+                    product.title = eventProduct.title
+                    product.image = eventProduct.image
+                    product.likes = eventProduct.likes
+
+                    await productRepository.save(product)
+                    console.log('Product created')
+                }, { noAck: true })
+
+                channel.consume('product_updated', async (msg) => {
+                    const eventProduct: Product = JSON.parse(msg.content.toString())
+                    const product = await productRepository.findOne({
+                        where: { admin_id: parseInt(eventProduct.id) }
+                    })
+
+                    productRepository.merge(product, {
+                        title: eventProduct.title,
+                        image: eventProduct.image,
+                        likes: eventProduct.likes,
+                    })
+
+                    await productRepository.save(product)
+                    console.log('Product updated')
+                }, { noAck: true })
+
+                channel.consume('product_deleted', async (msg) => {
+                    const admin_id = parseInt(msg.content.toString());
+
+                    await productRepository.deleteOne({ admin_id });
+
+                    console.log('Product deleted');
+                }, {noAck: true})
+
+
+                app.get('/api/products', async (req, res) => {
+                    const products = await productRepository.find()
+
+                    return res.send(products)
+                })
+
+                app.post('/api/products/:id/like', async (req, res) => {
+
+                    const productId = parseInt(req.params.id)
+                    const product  = await productRepository.findOne({where: {id: productId}})
+
+
+                    if(!product) {
+                        return res.status(404).json({
+                            message: "Product not found"
+                        })
+                    }
+
+                    // increase the likes 
+                    // in the admin app too
+                    await axios.post(`http://localhost:8000/api/products/${product.admin_id}/like`, {})
+
+                    product.likes++
+                    await productRepository.save(product)
+                    res.send(product)
+                })
+
+                app.listen(8001, () => {
+                    console.log('Server running at port 8001')
+                    console.log('Database Connected')
+                })
+
+                process.on('beforeExit', () => {
+                    console.log('closing')
+                    connection.close()
+                })
+            })
+
+        })
+    })
+    .catch((err) => {
+        console.error('Connection failed', err)
+    })
+
+
